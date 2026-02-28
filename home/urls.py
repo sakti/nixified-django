6b648040afc682d44a7baaa3efda351d9160be93
Sakti Dwi Cahyono <me@saktidwicahyono.name>
2026-02-28 20:49:07 +0700

    fix: remove prefix /
    Signed-off-by: Sakti Dwi Cahyono <me@saktidwicahyono.name>

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("/health", views.health, name="healthcheck"),
+    path("health", views.health, name="healthcheck"),
 ]
